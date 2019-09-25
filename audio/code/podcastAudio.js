2fd77235e22a63a7891be1e638add067e4472d49
John Colarusso <john.colarusso@samsung.com>
2019-09-25 12:58:12 -0700

    npr and removed capsule info

--- audio/code/podcastAudio.js
+++ audio/code/podcastAudio.js
@@ -2,15 +2,15 @@ const audioItems = [{
     id: 1,
     stream: [
       {
-        url: "https://storage.googleapis.com/bixby-audio-player-example/meows/203121_777645-lq.mp3",
+        url: "https://play.podtrac.com/npr-510289/edge1.pod.npr.org/anon.npr-podcasts/podcast/npr/pmoney/2019/09/20190920_pmoney_pmpod940v2-a6cdb7c9-15a2-465c-b820-04ce5cb7eff6.mp3?awCollectionId=510289&awEpisodeId=762748958&orgId=1&d=1170&p=510289&story=762748958&t=podcast&e=762748958&size=18685142&ft=pod&f=510289",
         format: "mp3"
       }
     ],
-    title: "Cat-ch Phrase",
-    subtitle: "meow.",
-    artist: "Cool Cat",
-    albumName: "Catatonic",
-    albumArtUrl: "https://storage.googleapis.com/bixby-audio-player-example/meows/cat-08.jpg",
+    title: "#940: Interest Rates... Why So Negative?",
+    subtitle: "Planet Money",
+    artist: "NPR",
+    albumName: "Planet Money",
+    albumArtUrl: "https://media.npr.org/assets/img/2018/08/02/npr_planetmoney_podcasttile_sq-7b7fab0b52fd72826936c3dbe51cff94889797a0.jpg?s=1400",
     tags: ["nature"]
   },
   {
